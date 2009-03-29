e510fd9c6f72beb26b88da89397ed9f6c48408d5
eman.nollase <eman.nollase@4135230c-1687-11de-b413-eb0469710719>
2009-03-29 10:44:28 +0000

    [eman]- added http service module

---
+++ trunk/wicket-jasper-ui/wicket-jasper-httpservice/src/main/java/com/ccti/jasper/http/service/JasperObject.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.ccti.jasper.http.service;
+
+import java.io.Serializable;
+
+
+/**
+ * @author Emmanuel A. Nollase - emanux
+ * @created Feb 26, 2009 - 3:07:15 PM
+ * 
+ */
+public class JasperObject implements Serializable
+{
+
+    private String username;
+    
+    private String password;
+    
+    private String reportId;
+    
+    public JasperObject()
+    {
+	// TODO Auto-generated constructor stub
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+    
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getReportId()
+    {
+        return reportId;
+    }
+
+    public void setReportId(String reportId)
+    {
+        this.reportId = reportId;
+    }
+    
+}
---
+++ trunk/wicket-jasper-ui/wicket-jasper-httpservice/src/main/java/com/ccti/jasper/http/service/JasperSingle.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.ccti.jasper.http.service;
+
+import java.io.Serializable;
+
+
+/**
+ * @author Emmanuel A. Nollase - emanux
+ * @created Feb 27, 2009 - 3:21:31 PM
+ * 
+ */
+public class JasperSingle implements Serializable
+{
+
+    private JasperObject jasperObject;
+    
+    private static JasperSingle jasperSingle;
+    
+    private JasperSingle()
+    {
+	
+    }
+    
+    public static JasperSingle getInstance()
+    {
+	if(null == jasperSingle)
+	{
+	    jasperSingle = new JasperSingle();
+	}
+	
+	return jasperSingle;
+    }
+
+    
+    public JasperObject getJasperObject()
+    {
+        return jasperObject;
+    }
+
+    
+    public void setJasperObject(JasperObject jasperObject)
+    {
+        this.jasperObject = jasperObject;
+    }
+    
+    
+}
