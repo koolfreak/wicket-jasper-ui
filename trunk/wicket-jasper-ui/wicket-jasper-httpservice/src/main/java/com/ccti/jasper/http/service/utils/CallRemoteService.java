de6865bc43335a1af248303bd9ea070006fdae6f
eman.nollase <eman.nollase@4135230c-1687-11de-b413-eb0469710719>
2009-03-29 10:49:41 +0000

    [eman]- added http service module

---
+++ trunk/wicket-jasper-ui/wicket-jasper-httpservice/src/main/java/com/ccti/jasper/http/service/utils/CallRemoteService.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ccti.jasper.http.service.utils;
+
+
+/**
+ * @author Emmanuel A. Nollase - emanux
+ * @created Mar 2, 2009 - 11:12:17 AM
+ * 
+ */
+public interface CallRemoteService
+{
+    /**
+     * @return -  <code>true</code> if authenticated from remote report server, <code>false</code> otherwise
+     */
+    boolean callRemoteLogin();
+    
+    /**
+     * Logout user from remote report server
+     * @param id - assigned user report id
+     * @return 
+     */
+    boolean callRemoteLogout(final String id);
+    
+    String jasperServerURL();
+    
+}
