6c62228b8a9100a2aceea6ceff80e3d9b25038c7
eman.nollase <eman.nollase@4135230c-1687-11de-b413-eb0469710719>
2009-04-06 12:09:11 +0000

    added files

---
+++ wicket-jasper-ui/wicket-jasper-core/src/main/java/wicket/contrib/jasperreports/link/JRTextLink.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package wicket.contrib.jasperreports.link;
+
+import wicket.contrib.jasperreports.JRTextResource;
+
+
+/**
+ * @author Emmannuel A. Nollase
+ * Created: Apr 6, 2009 - 4:16:23 PM
+ */
+public abstract class JRTextLink extends JRResourceExportLink
+{
+
+    public JRTextLink(String id)
+    {
+	super(id);
+	// TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.wicket.markup.html.link.Link#onClick()
+     */
+    @Override
+    public void onClick()
+    {
+	JRTextResource resource = new  JRTextResource(getReportFile());
+	resource.setReportDataSource(getSource());
+	resource.setReportParameters(getParams());
+	
+	resource.onResourceRequested();
+
+    }
+
+}
