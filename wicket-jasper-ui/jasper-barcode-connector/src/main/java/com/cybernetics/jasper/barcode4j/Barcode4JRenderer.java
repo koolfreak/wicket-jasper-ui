1fe2c93cbbb395dfc5a20a1e7fe79e4ee1ce1b6f
eman.nollase <eman.nollase@4135230c-1687-11de-b413-eb0469710719>
2009-10-29 10:02:59 +0000

    add barcode4j connector

---
+++ wicket-jasper-ui/jasper-barcode-connector/src/main/java/com/cybernetics/jasper/barcode4j/Barcode4JRenderer.java
@@ -0,0 +1,17 @@
+// Decompiled by DJ v2.8.8.54 Copyright 2000 Atanas Neshkov  Date: 4/8/2009 12:01:26 PM
+// Home Page : http://members.fortunecity.com/neshkov/dj.html  - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Barcode4JJasperRender.java
+
+package com.cybernetics.jasper.barcode4j;
+
+import net.sf.jasperreports.engine.JRImageRenderer;
+
+public class Barcode4JRenderer extends JRImageRenderer
+{
+
+    public Barcode4JRenderer(byte[] imageData)
+    {
+        super(imageData);
+    }
+}
